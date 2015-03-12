0a62809dce36c01fad0e6aff541b464276dfd985
Jonathan Eastman <jonathan.eastman@gmail.com>
2015-03-11 21:45:07 -0500

    Initial upload to city server

--- contact.php
+++ contact.php
@@ -1,20 +1,20 @@
-<?php
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
-    $from = 'From: cityofwaterlooiowa.com'; 
-    $to = 'chiefsec@waterloo-ia.org'; 
-    $subject = 'Online inquiry for WFR';
+<?php //
+ //   $name = $_POST['name'];
+//    $email = $_POST['email'];
+ //   $message = $_POST['message'];
+//    $from = 'From: cityofwaterlooiowa.com'; 
+//    $to = 'chiefsec@waterloo-ia.org'; 
+ //   $subject = 'Online inquiry for WFR';
 
-    $body = "From: $name\n E-Mail: $email\n Message:\n $message";
+ //   $body = "From: $name\n E-Mail: $email\n Message:\n $message";
 ?>
 
 <?php
-if ($_POST['submit']) {
-    if (mail ($to, $subject, $body, $from)) { 
-        echo '<p>Message has been sent and will be promptly processed.</p>';
-    } else { 
-        echo '<p>An error has occurred: please attempt re-sending the message.</p>'; 
-    }
-}
+//if ($_POST['submit']) {
+//    if (mail ($to, $subject, $body, $from)) { 
+ //       echo '<p>Message has been sent and will be promptly processed.</p>';
+ //   } else { 
+  //      echo '<p>An error has occurred: please attempt re-sending the message.</p>'; 
+  //  }
+//}
 ?>
